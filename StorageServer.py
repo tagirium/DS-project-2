a690d4724ccde361c5edb46aa2002f550dd11e91
tagirium <shtagir@mail.ru>
2020-10-09 20:35:46 +0300

    storage server without commands writing in txt, codes added, first version of pings

--- StorageServer.py
+++ StorageServer.py
@@ -1,18 +1,19 @@
 import os
 import shutil
-import socket
+from help import *
+from codes import *
 
 
 class StorageServer:
 	def file_read(self, path):
-		self.__send_file(path)
+		self.send_file(path)
 
 	def file_create(self, path):
 		file = open(path, 'w+')
 		file.close()
 
-	def file_write(self, path):  # ?
-		self.__receive_file(path)
+	def file_write(self, path):
+		self.receive_file(path)
 
 	def file_delete(self, path):
 		if os.path.exists(path):
@@ -46,9 +47,9 @@ class StorageServer:
 		if os.path.exists(path):
 			os.rmdir(path)
 
-	def __receive_file(self, path):
+	def receive_file(self, path):
 		ssFT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		ssFT.bind((socket.gethostname(), 8756))
+		ssFT.bind((socket.gethostname(), 8801))
 		ssFT.listen(1)
 		while True:
 			(conn, address) = ssFT.accept()
@@ -57,7 +58,7 @@ class StorageServer:
 			# Receive, output and save file
 			with open(text_file, "wb") as fw:
 				while True:
-					data = conn.recv(32)
+					data = conn.recv(BUFFER_SIZE)
 					if not data:
 						break
 					else:
@@ -66,30 +67,31 @@ class StorageServer:
 			break
 		ssFT.close()
 
-	def __send_file(self, path):
+	def send_file(self, path):
 		ssFT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		ssFT.bind((socket.gethostname(), 8758))
+		ssFT.bind((socket.gethostname(), STORAGE_SERVER_PORT))
 		ssFT.listen(1)
 		while True:
 			(conn, address) = ssFT.accept()
 			with open(path, 'ab+') as fa:
-				l = fa.read(32)
+				l = fa.read(BUFFER_SIZE)
 				while l:
 					conn.send(l)
-					l = fa.read(32)
+					l = fa.read(BUFFER_SIZE)
 				fa.close()
 			break
+		ssFT.close()
 
 	def receive_path(self):
 		ssFT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		ssFT.bind((socket.gethostname(), 8757))
+		ssFT.bind((socket.gethostname(), STORAGE_SERVER_PORT))
 		ssFT.listen(1)
 		path = ''  # path
 		while True:
 			(conn, address) = ssFT.accept()
 			# Receive, output and save file
 			while True:
-				data = conn.recv(32)
+				data = conn.recv(BUFFER_SIZE)
 				if not data:
 					break
 				else:
@@ -97,3 +99,4 @@ class StorageServer:
 			break
 		ssFT.close()
 		return path
+
